d6f7e373b605d4a96b997526c143f2810d9038e8
Cristina Espinosa Colomé <cespinosaco@uoc.edu>
2026-04-25 22:09:29 +0200

    Pantalla d'inici i redirect de login

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,17 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
-use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 use App\Http\Controllers\TicketController;
 use App\Http\Controllers\CommentController;
 use App\Http\Controllers\DashboardController;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+    if (auth()->check()) {
+        return redirect()->route('tickets.index');
+    }
+
+    return redirect()->route('login');
 });
 
 Route::get('/dashboard', function () {
